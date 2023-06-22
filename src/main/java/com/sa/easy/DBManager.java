804f1dec26afe0536476cb27257ac8a88c4acab6
Koungmin Kwon <kkyeongmin123@gmail.com>
2023-06-22 18:31:46 +0900

    장바구니 js 관련 수정사항 병합

--- src/main/java/com/sa/easy/DBManager.java
+++ src/main/java/com/sa/easy/DBManager.java
@@ -15,8 +15,8 @@ public class DBManager {
 
 	// db�۾��ÿ� ��¶�� ���� �ؾߵ�
 	public static Connection connect() throws SQLException {
-		String url = "jdbc:oracle:thin:@qwywatff3dircwtm_high?TNS_ADMIN=C:/jsa/Wallet_QWYWATFF3DIRCWTM";
-		System.out.println("���� ����!");
+		String url = "jdbc:oracle:thin:@qwywatff3dircwtm_high?TNS_ADMIN=C:/SBT7/dbws/Wallet_QWYWATFF3DIRCWTM";
+		System.out.println("연결 성공!");
 		return DriverManager.getConnection(url, "ADMIN", "Soldesk802!!");
 	}
 
