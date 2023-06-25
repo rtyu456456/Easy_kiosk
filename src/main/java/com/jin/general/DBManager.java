0ea5605a6b72a6e485cc9a10c25874c0926f81e6
rtyu456456 <77466225+rtyu456456@users.noreply.github.com>
2023-06-25 20:56:20 +0900

    합치기 전 불필요한 파일 1차 정리

--- src/main/java/com/jin/general/DBManager.java
+++ src/main/java/com/jin/general/DBManager.java
@@ -14,7 +14,7 @@ public class DBManager {
 	
 	// db�۾��ÿ� ��¶�� ���� �ؾߵ�
 	public static Connection connect() throws SQLException {
-		String url = "jdbc:oracle:thin:@qwywatff3dircwtm_high?TNS_ADMIN=C:/Wallet_QWYWATFF3DIRCWTM";
+		String url = "jdbc:oracle:thin:@qwywatff3dircwtm_high?TNS_ADMIN=C:/kkh/eclipseDB/Wallet_QWYWATFF3DIRCWTM";
 		System.out.println("연결/실행 성공");
 		return DriverManager.getConnection(url, "ADMIN", "Soldesk802!!");
 	}
